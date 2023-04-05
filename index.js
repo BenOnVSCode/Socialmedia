3c352a531321244c7e46409652b336d0d48ddaf4
4bdeladim <abdeladim.frontend@gmail.com>
2023-04-05 20:56:16 +0000

    build

--- index.js
+++ index.js
@@ -35,11 +35,11 @@ mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTo
     console.log("Mongo connected !")
 });
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+
+app.use(express.static('client/build'))
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
+
 
 app.listen(PORT)
